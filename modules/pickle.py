7db86044cecd5ad04c55dbebe10e1f794ea6823a
Julia Zahorovska <ju.zagorovsky@gmail.com>
2024-06-01 16:52:32 +0200

    add pickle

---
+++ modules/pickle.py
@@ -0,0 +1,13 @@
+import pickle
+from address_book import AddressBook
+
+def save_data(book, filename="addressbook.pkl"):
+    with open(filename, "wb") as f:
+        pickle.dump(book, f)
+
+def load_data(filename="addressbook.pkl"):
+    try:
+        with open(filename, "rb") as f:
+            return pickle.load(f)
+    except FileNotFoundError:
+        return AddressBook()
